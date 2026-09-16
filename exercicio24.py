579bdd5e6910130b1bcf1b377070772bd5b4167f
Guilherme <guilhermeimamura2008@gmail.com>
2026-09-15 21:58:32 -0300

    Add leap year check in exercicio24.py

--- exercicio24.py
+++ exercicio24.py
@@ -1,1 +1,9 @@
+# Leitura do ano inteiro
+ano = int(input("Ano: "))
+
+# Verificação da regra utilizando estruturas condicionais
+if (ano % 400 == 0) or (ano % 4 == 0 and ano % 100 != 0):
+    print("Resultado: ANO BISSEXTO")
+else:
+    print("Resultado: NÃO BISSEXTO")
 
