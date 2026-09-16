fcce41163e08a82ef6c5e0fde8f7a03d5ba6fde4
Guilherme <guilhermeimamura2008@gmail.com>
2026-09-15 22:05:52 -0300

    Implement triangle existence and classification logic
    Add triangle classification based on side lengths

--- exercicio29.py
+++ exercicio29.py
@@ -1,1 +1,17 @@
+# Leitura dos três lados
+a = float(input("Digite o primeiro lado: "))
+b = float(input("Digite o segundo lado: "))
+c = float(input("Digite o terceiro lado: "))
+
+# Verificação da condição de existência do triângulo
+if (a + b > c) and (a + c > b) and (b + c > a):
+    # Classificação do triângulo
+    if a == b == c:
+        print("EQUILÁTERO")
+    elif a == b or a == c or b == c:
+        print("ISÓSCELES")
+    else:
+        print("ESCALENO")
+else:
+    print("NÃO FORMA TRIÂNGULO")
 
